2d9e0bb5f9a4465fb736655abbccfd7768df9a23
y3owk1n <wongyeowkin@gmail.com>
2022-11-09 02:14:42 +0800

    eslint formatting

--- .config/nvim/lua/kyle/core/options.lua
+++ .config/nvim/lua/kyle/core/options.lua
@@ -11,6 +11,7 @@ opt.number = true -- shows absolute line number on cursor line (when relative nu
 -- tabs & indentation
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.softtabstop = 2 -- 2 spaces for softtab
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
