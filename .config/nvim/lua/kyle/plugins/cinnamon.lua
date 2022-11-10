7c8e208a8a1316d889f3643f5027b1a7f7fdc248
y3owk1n <wongyeowkin@gmail.com>
2022-11-10 16:55:25 +0800

    update: smooth scroll speed in nvim

--- .config/nvim/lua/kyle/plugins/cinnamon.lua
+++ .config/nvim/lua/kyle/plugins/cinnamon.lua
@@ -15,7 +15,7 @@ cinnamon.setup({
 	-- OPTIONS:
 	always_scroll = false, -- Scroll the cursor even when the window hasn't scrolled.
 	centered = true, -- Keep cursor centered in window when using window scrolling.
-	default_delay = 7, -- The default delay (in ms) between each line when scrolling.
+	default_delay = 2, -- The default delay (in ms) between each line when scrolling.
 	hide_cursor = false, -- Hide the cursor while scrolling. Requires enabling termguicolors!
 	horizontal_scroll = true, -- Enable smooth horizontal scrolling when view shifts left or right.
 	max_length = -1, -- Maximum length (in ms) of a command. The line delay will be
