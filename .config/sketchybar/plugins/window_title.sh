dc8342e86342a9a4e9af3d28f9ecfa829a21c303
y3owk1n <wongyeowkin@gmail.com>
2022-11-21 11:55:29 +0800

    fixed some issue

--- .config/sketchybar/plugins/window_title.sh
+++ .config/sketchybar/plugins/window_title.sh
@@ -6,8 +6,8 @@ if [[ $WINDOW_TITLE = "" ]]; then
     WINDOW_TITLE=$(/opt/homebrew/bin/yabai -m query --windows --window | jq -r '.app')
 fi
 
-if [[ ${#WINDOW_TITLE} -gt 55 ]]; then
-    WINDOW_TITLE=$(echo "$WINDOW_TITLE" | cut -c 1-55)
+if [[ ${#WINDOW_TITLE} -gt 60 ]]; then
+    WINDOW_TITLE=$(echo "$WINDOW_TITLE" | cut -c 1-60)
     sketchybar -m --set title label="$WINDOW_TITLE"…
     exit 0
 fi
