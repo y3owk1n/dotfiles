18771528998a6f55e45f89694a7a773f51ee8002
y3owk1n <wongyeowkin@gmail.com>
2022-11-24 03:32:30 +0800

    updated cmp

--- .config/nvim/lua/kyle/plugins/nvim-cmp.lua
+++ .config/nvim/lua/kyle/plugins/nvim-cmp.lua
@@ -14,11 +14,9 @@ end
 -- load vs-code like snippets from plugins (e.g. friendly-snippets)
 require("luasnip/loaders/from_vscode").lazy_load()
 
--- vim.opt.completeopt = "menu,menuone,noselect"
-
-local check_backspace = function()
-	local col = vim.fn.col(".") - 1
-	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 cmp.setup({
@@ -44,7 +42,7 @@ cmp.setup({
 				luasnip.expand()
 			elseif luasnip.expand_or_jumpable() then
 				luasnip.expand_or_jump()
-			elseif check_backspace() then
+			elseif has_words_before() then
 				fallback()
 			else
 				fallback()
