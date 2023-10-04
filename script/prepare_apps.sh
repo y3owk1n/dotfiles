802ed340fa31ff89cd18ca5a24aa7c2d56b98984
Kyle Wong <wongyeowkin@gmail.com>
2023-10-04 22:47:35 +0800

    chore (script): add git-credential-manager-core to install list

--- script/prepare_apps.sh
+++ script/prepare_apps.sh
@@ -8,11 +8,11 @@ echo -e "🖐️ --- Checking Homebrew... --------------------------------------
 
 # Check if Homebrew is installed, and install it if not
 if ! command -v brew &>/dev/null; then
-    echo -e "\t ❌ Homebrew is not installed. Installing..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-    echo -e "\t ✅ Homebrew is installed successfully"
+	echo -e "\t ❌ Homebrew is not installed. Installing..."
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+	echo -e "\t ✅ Homebrew is installed successfully"
 else
-    echo -e "\t ✅ Homebrew is already installed"
+	echo -e "\t ✅ Homebrew is already installed"
 fi
 
 echo -e "✅ --- Homebrew Checks Completed --------------------------------------------------"
@@ -21,40 +21,41 @@ echo
 
 # List of applications to install via Homebrew
 apps_to_install=(
-    alacritty
-    autojump
-    bat
-    bun
-    font-sf-mono-nerd-font-ligaturized
-    karabiner-elements
-    lazygit
-    lsd
-    luajit
-    minio
-    neovim
-    nvm
-    orbstack
-    pnpm
-    pyenv
-    rm-improved
-    skhd
-    spaceid
-    yabai
-    yadm
-    yamlfmt
-    yarn
+	alacritty
+	autojump
+	bat
+	bun
+	font-sf-mono-nerd-font-ligaturized
+	git-credential-manager-core
+	karabiner-elements
+	lazygit
+	lsd
+	luajit
+	minio
+	neovim
+	nvm
+	orbstack
+	pnpm
+	pyenv
+	rm-improved
+	skhd
+	spaceid
+	yabai
+	yadm
+	yamlfmt
+	yarn
 )
 
 echo -e "🖐️  --- Preparing Homebrew Apps ----------------------------------------------------"
 
 # Loop through the list and install the applications
 for app in "${apps_to_install[@]}"; do
-    if brew list "$app" &>/dev/null; then
-        echo -e "\t ✅ $app is already installed."
-    else
-        echo -e "\t Installing $app..."
-        brew install "$app"
-    fi
+	if brew list "$app" &>/dev/null; then
+		echo -e "\t ✅ $app is already installed."
+	else
+		echo -e "\t Installing $app..."
+		brew install "$app"
+	fi
 done
 
 echo -e "✅ --- All Homebrew apps are installed --------------------------------------------"
@@ -68,21 +69,23 @@ source ~/.nvm/nvm.sh
 
 # Check if Node.js is already installed
 if ! nvm ls | grep -q "lts/*"; then
-    # Install the latest LTS version of Node.js
-    echo -e "\t Installing the latest LTS version of Node.js..."
-    nvm install --lts
+	# Install the latest LTS version of Node.js
+	echo -e "\t Installing the latest LTS version of Node.js..."
+	nvm install --lts
 
-    # Set the latest LTS version as the default
-    nvm alias default "lts/*"
-    echo -e "\t ✅ Node.js (LTS) is installed succesfully."
+	# Set the latest LTS version as the default
+	nvm alias default "lts/*"
+	echo -e "\t ✅ Node.js (LTS) is installed succesfully."
 else
-    echo -e "\t ✅ Node.js (LTS) is already installed."
+	echo -e "\t ✅ Node.js (LTS) is already installed."
 fi
 
 # Display Node.js and NPM versions
-echo -n -e "\t Node.js version: "; node -v
+echo -n -e "\t Node.js version: "
+node -v
 
-echo -n -e "\t NPM version: "; npm -v
+echo -n -e "\t NPM version: "
+npm -v
 
 echo -e "✅ --- NVM setup completed --------------------------------------------------------"
 
