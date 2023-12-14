caccb8ee8478cb58186486c664cabd26c85205d0
mtraworld <140996996+mtraworld@users.noreply.github.com>
2023-12-14 13:16:56 +0800

    update dots (#21)

---
+++ .config/zsh/alias.zsh
@@ -0,0 +1,26 @@
+# general
+alias c="clear"
+alias x="exit"
+alias config="cd $HOME/.config && nvim ."
+alias zc="cd $HOME/.config/zsh && nvim ."
+alias zr="source $HOME/.zshrc"
+alias nvimc="cd $HOME/.config/nvim && nvim ."
+alias ..="cd .."
+alias ...="cd ../.."
+
+# obsidian folder
+alias obs-kyle="cd $HOME/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Kyle/ && nvim ."
+alias obs-traworld="cd $HOME/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Traworld/ && nvim ."
+
+# brew
+alias brewup='brew update; brew upgrade; brew cleanup; brew doctor'
+
+# ls alias to exa
+alias ls="lsd -a"
+alias lsa="lsd -la"
+
+# cat to bat
+alias cat="bat -pp"
+
+# lazy git
+alias gg="lazygit"
