493131522674d505565396f1f40f275c1297756c
mtraworld <140996996+mtraworld@users.noreply.github.com>
2023-12-18 12:05:49 +0800

    chore (nvim): convert back to catppuccin transparent background

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -30,7 +30,7 @@ return {
                 light = "latte",
                 dark = "macchiato",
             },
-            transparent_background = false, -- disables setting the background color.
+            transparent_background = true, -- disables setting the background color.
             show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
             term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
             dim_inactive = {
@@ -56,12 +56,7 @@ return {
                 operators = {},
             },
             color_overrides = {},
-            custom_highlights = function(colors)
-                return {
-                    NeoTreeNormal = { fg = colors.text, bg = colors.none },
-                    NeoTreeNormalNC = { fg = colors.text, bg = colors.none },
-                }
-            end,
+            custom_highlights = {},
             integrations = {
                 aerial = true,
                 alpha = true,
